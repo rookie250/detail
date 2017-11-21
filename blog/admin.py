70c6b323cf8ec7f3d9ed3baeae98b03cfb3eb034
rookie250 <mmix1009@163.com>
2017-11-21 19:54:36 +0800

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Post, Category, Tag
 
 
+
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
