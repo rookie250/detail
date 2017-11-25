f3bc09e0aacbd3c285f2098ee32770a17db204dd
rookie250 <zhouming@zhou.local>
2017-11-25 15:25:56 +0800

    commit

--- comments/models.py
+++ comments/models.py
@@ -1,24 +1,17 @@
 from django.db import models
+from django.utils.six import python_2_unicode_compatible
 
-# Create your models here.
+
+# python_2_unicode_compatible 装饰器用于兼容 Python2
+@python_2_unicode_compatible
 class Comment(models.Model):
-    name = models.CharField(max_length=70)
+    name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255)
     url = models.URLField(blank=True)
     text = models.TextField()
-    created_time = models.DateField(auto_now=True)  # 表示将评论的当前时间保存到数据库
+    created_time = models.DateTimeField(auto_now_add=True)
 
     post = models.ForeignKey('blog.Post')
 
     def __str__(self):
         return self.text[:20]
-
-### 关于 self.text[:20]语法和字符串截取的语法一样。用demo来演示
-
-### >>> str = '0123456789'
-### >>> print (str[0:3])
-### 012
-### >>> print (str[:3])
-### 012
-### >>>
-
